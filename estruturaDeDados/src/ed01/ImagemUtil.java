e20bd729716e7b9c70812773bcba235ccdd3441c
EduardoValoisFranz <38699565+eduardovaloisfranz@users.noreply.github.com>
2020-02-24 17:16:43 -0300

    desisto
    É IMPOSSIVEL VELHO, JA TENTEI DE TUDO

--- estruturaDeDados/src/ed01/ImagemUtil.java
+++ estruturaDeDados/src/ed01/ImagemUtil.java
@@ -1,10 +1,61 @@
 package ed01;
 
-public class ImagemUtil {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-	public static void main(String[] args) {
-		ReadInfoUser.readString(message)
-
+public class ImagemUtil {	
+	
+		
+	public static ArrayList<Image> patterns = new ArrayList<>();
+	
+	
+	public static boolean verificaPadrao(Image imagem, int padrao) {
+		int counter = 0, counterLine = 0;
+		int linesWasFound[] = new int[3];
+		loadPattern();
+		for(int line = 0; line < imagem.getImage().length; line++) {
+			for(int column = 0; column < imagem.getImage().length; column++) {
+				//if(imagem.getImage()[line][column] == patterns.get(1).getImage()[line][column]) {					
+				//}				
+				//counter++;
+				if(Arrays.asList(imagem).contains(patterns.get(padrao))){
+					return true;
+				}				
+			}
+		}
+		
+		return false;
 	}
+	
+	public static int contarPadrao(Image imagem, int padrao) {
+		return 1;
+	}
+	
+	
+	public static void loadPattern() {
+		if(patterns.isEmpty()) {
+			Image pattern1 = new Image();
+			pattern1.addLine("010", 0);
+			pattern1.addLine("111", 1);
+			pattern1.addLine("010", 2);
+			patterns.add(pattern1);
+			
+			Image pattern2 = new Image();
+			pattern2.addLine("111", 0);
+			pattern2.addLine("010", 1);
+			pattern2.addLine("010", 2);
+			patterns.add(pattern2);
+			
+			Image pattern3 = new Image();
+			pattern3.addLine("001", 0);
+			pattern3.addLine("010", 1);
+			pattern3.addLine("100", 2);
+			patterns.add(pattern3);	
+		}
+		
+				
+			
+	}
+	
 
 }
--- estruturaDeDados/src/ed01/ReadInfoUser.java
+++ estruturaDeDados/src/ed01/ReadInfoUser.java
@@ -7,17 +7,29 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReadInfoUser {
-	public static String getInfoTxtFromPath(String pathTxt) {
-		String imageArray = "";
-
+	public static Image getInfoTxtFromPath(String pathTxt) {
+		Image image = new Image();
 		String path = "";
-		do {
-			System.out.println("Please insert a TXT file ONLY!");
-			path = ReadInfoUser.readString("Insert only path that contains a TXT to read");
-		} while (path.toLowerCase().endsWith(".txt"));
+		if(pathTxt.toLowerCase().endsWith(".txt")) {
+			path = pathTxt;
+		}else {
+			do {
+				System.out.println("Please insert a TXT file ONLY!");
+				path = ReadInfoUser.readString("Insert only path that contains a TXT to read");
+			} while (!path.toLowerCase().endsWith(".txt"));			
+		}
+	
 
 		String fileName = path;
 		File file = new File(fileName);
+		if(!file.exists()) {
+			do {
+				System.out.println("That file doesnt exists!");
+				path = ReadInfoUser.readString("Please: insert a directory that exists in your computer");
+				fileName = path;
+			} while (!file.exists());	
+			file = new File(fileName);
+		}
 		try {
 
 			FileReader fr = new FileReader(file);
@@ -27,27 +39,34 @@ public class ReadInfoUser {
 			while ((line = br.readLine()) != null) {
 				if(line.isEmpty()) {
 					System.out.println("The file is empty");
-				}else if(line.length() != 17) {
+				}else if(line.length() != 16) {
 					System.out.println("The file have invalid format, try put in the right pattern");
-				}else if (counter > 1) {
-					System.out.println("Your txt contains more than one pattern, the image that will be used is: " + imageArray + " the: " + line + " is ignorated");
+				} else if (!line.contains("1") && !line.contains("0")) {
+					System.out.println("Invalid characters, the line contains invalid char, only insert 1 and 0");
 				}
-				
+				else {
+					image.addLine(line, counter);					
+				}				
 				counter++;
 			}
+			br.close();
+			fr.close();			
 
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		return imageArray;
+		return image;
 
 	}
 
 	public static String readString(String message) {
 		Scanner input = new Scanner(System.in);
 		String text = input.nextLine();
+		input.close();
 		return text;
+		
 
 	}
 }
