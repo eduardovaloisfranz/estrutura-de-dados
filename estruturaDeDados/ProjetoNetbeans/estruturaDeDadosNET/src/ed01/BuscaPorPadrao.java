1079c9c734c7d55f67783e3b2365f952a21a074c
Your Name <you@example.com>
2020-02-27 21:23:05 -0300

    sla

--- estruturaDeDados/ProjetoNetbeans/estruturaDeDadosNET/src/ed01/BuscaPorPadrao.java
+++ estruturaDeDados/ProjetoNetbeans/estruturaDeDadosNET/src/ed01/BuscaPorPadrao.java
@@ -4,6 +4,20 @@ package ed01;
 public class BuscaPorPadrao {
 	public static void main(String[] args) {		
 		
+            
+            
+            if(args.length != 2 || args.length != 3){
+                if((args[0].equals("-location")) && (args[1].contains("C;"))){
+                    String path = args[1];
+                    //int pattern = Integer.parseInt(args[2]);
+                    if(args.length >= 3){
+                        if()
+                    }
+                    
+                }
+            }
+            
+            /*
 		Image imageUser = ReadInfoUser.getInfoTxtFromPath("C:\\Users\\casa\\Documents\\GitHub\\estrutura-de-dados\\estruturaDeDados\\src\\ed01\\vectors.txt");		
 		
                System.out.println(ImagemUtil.verificaPadrao(imageUser, 1));
@@ -23,6 +37,7 @@ public class BuscaPorPadrao {
                     System.out.print("\n");
                     
                 }
+               */
             
                         
 		
