821ac5f24795ebd34a3492db7f6a115d3a5ba5d6
EduardoValoisFranz <38699565+eduardovaloisfranz@users.noreply.github.com>
2020-03-14 15:47:43 -0300

    Fim LinkedList

--- estruturaDeDados/src/implementacaoListaComEncadeamento/ListaComEncadeamento.java
+++ estruturaDeDados/src/implementacaoListaComEncadeamento/ListaComEncadeamento.java
@@ -1,6 +1,6 @@
 package implementacaoListaComEncadeamento;
 
-public class ListaComEncadeamento {
+public class ListaComEncadeamento{
 	private NoLista first;
 	private NoLista last;
 	private int counter;
@@ -12,42 +12,247 @@ public class ListaComEncadeamento {
 	}
 	
 	public void add(Integer info) {
-		if(counter == 0) {
-			this.first = new NoLista(info, null);
-			this.last = first;
+		if(info == null) {
+			throw new NullPointerException("You cant give null info");
 		}else {
-			NoLista novo = new NoLista(info, null);
-			this.last.setNext(novo);
-			this.last = novo;
+			if(counter == 0) {
+				this.first = new NoLista(info, null);
+				this.last = first;
+			}else {
+				NoLista novo = new NoLista(info, null);
+				this.last.setNext(novo);
+				this.last = novo;
+			}
+			this.counter++;	
 		}
-		this.counter++;
+		
 	}
 	
 	public void add(Integer info, int index) {
+		if(info == null) {
+			throw new NullPointerException("You cant give null info");
+		}
+		if(index < 0 || index > counter) {
+			throw new IndexOutOfBoundsException("The position is invalid");
+		}
+		
 		int counter = 0;
-		NoLista temp = first;
-		while(temp.getNext() != null) {
-			NoLista novo = new NoLista();		
+		NoLista temp = first;	
+			
+		NoLista elementByIndexAsc = null;
+		NoLista elementByIndex= null;
+		while(temp != null) {				
+			if(counter == index - 1) {
+				elementByIndexAsc = temp;
+			}			
 			if(counter == index) {
-				novo.setNext(temp);
+				elementByIndex = temp;
+			}
+			counter++;
+			temp = temp.getNext();
+		}
+		NoLista novo = new NoLista(info, elementByIndex);
+		elementByIndexAsc.setNext(novo);
+		this.counter++;
+	}
+	
+	public Integer remove(int index) {
+		if(index < 0 || index > counter) {
+			throw new IndexOutOfBoundsException("Invalid index");
+		}
+		Integer oldValue = null;
+		NoLista temp = first;
+		NoLista previousElement = null;
+		NoLista nextElement = null; 
+		int counter = 0;
+		while(temp != null) {				
+			if(counter == index - 1) {
+				previousElement = temp;
+			}
+			if(counter == index + 1) {
+				nextElement = temp;
+			}			
+			counter++;
+			temp = temp.getNext();
+		}
+		oldValue = previousElement.getNext().getInfo();
+		previousElement.setNext(nextElement);
+		this.counter--;		   
+		
+		return oldValue;
+	}
+	
+	public boolean removeFirst(Integer info) {
+		boolean infoExists = false;
+		if(info == null) {
+			throw new NullPointerException("You cant give null info");
+		}
+		NoLista temp = first;
+		int counter = 0;
+		while(temp != null) {
+			if(info.equals(temp.getInfo())) {
+				remove(counter);
+				infoExists = true;
+			}
+			counter++;
+			temp = temp.getNext();
+		}	
+		return infoExists;	
+		
+	}
+	
+	public Integer get(int index) {
+		if(index < 0 || index > counter) {
+			throw new IndexOutOfBoundsException("Invalid index");
+		}		
+		Integer info = null;
+		NoLista temp = first;
+		int counter = 0;
+		while(temp != null) {
+			if(index == counter) {
+				info = temp.getInfo();
 			}
 			
 			counter++;
 			temp = temp.getNext();
+		}	
+		return info;
+	}
+	
+	public Integer set(int index, Integer info) {
+		if(index < 0 || index > counter) {
+			throw new IndexOutOfBoundsException("Invalid Index");
+		}
+		if(info == null) {
+			throw new NullPointerException("You cant give null info");
+		}
+		Integer oldInfo = null;
+		NoLista temp = first;
+		int counter = 0;
+		while(temp != null) {
+			if(index == counter) {
+				oldInfo = temp.getInfo();
+				temp.setInfo(info);
+			}			
+			counter++;
+			temp = temp.getNext();			
+		}		
+		return oldInfo;
+	}
+	
+	public boolean isEmpty() {
+		if(counter == 0) {
+			return true;
+		}else {
+			return false;
 		}
 	}
+	
+	public boolean contains(Integer info) {
+		if(info == null) {
+			throw new NullPointerException("You cant give null info");
+		}
+		NoLista temp = first;
+		while(temp != null) {
+			if(temp.getInfo().equals(info)) {
+				return true;
+			}
+			temp = temp.getNext();
+		}
+		return false;
+	}
+	
+	public int indexOf(Integer info) {
+		if(info == null) {
+			throw new NullPointerException("You cant give null info");			
+		}
+		NoLista temp = first;
+		int counter = 0;
+		while(temp != null) {
+			if(temp.getInfo().equals(info)) {
+				return counter;
+			}
+			counter++;
+			temp = temp.getNext();
+		}
+	
+		return -1;
+	}
+	
+	private NoLista getNoListaByIndex (int index) {
+		if(index < 0 || index > counter) {
+			throw new IndexOutOfBoundsException("Invalid index");
+		}		
+		NoLista noLista = null;
+		NoLista temp = first;
+		int counter = 0;
+		while(temp != null) {
+			if(index == counter) {
+				noLista = temp;
+			}
+			
+			counter++;
+			temp = temp.getNext();
+		}	
+		return noLista;
+	}
+	
+	public int lastIndexOf(Integer info) {
+		if(info == null) {
+			throw new NullPointerException("You cant give null info");			
+		}
+		if(indexOf(info) == -1) {
+			return -1;
+		}
+		int firstIndex = indexOf(info);
+		int lastIndex = 0;		
+		NoLista temp = getNoListaByIndex(firstIndex);
+		while(temp != null) {
+			if(temp.getInfo().equals(info)) {
+				lastIndex = firstIndex;
+			}
+			firstIndex++;
+			temp = temp.getNext();			
+		}
+		if(lastIndex == 0) {
+			return -1;	
+		}else {
+			return lastIndex;
+		}
+		
+		
+	}
+	
+	public Integer[] toArray() {
+		Integer[] arr = new Integer[counter];
+		NoLista temp = first;
+		int counter = 0;
+		while(temp != null){
+			arr[counter] = temp.getInfo();
+			
+			counter++;
+			temp = temp.getNext();			
+		}
+		return arr;
+	}
+	
 	
 	public int size() {
 		return this.counter;
 	}
 	
-	public void exibirLista() {		
+	@Override
+	public String toString() {		
+		String info = " [ ";
 		NoLista temp = first;
 		do {
-			System.out.println("Atual: " + temp + " Proximo: " + temp.getNext());
+			//System.out.println("Atual: " + temp + " Proximo: " + temp.getNext());
+			info += "{ Atual: " + temp + " Proximo: " + temp.getNext() + " }";
 			temp = temp.getNext();
 			
-		}while(temp.getNext() != null);
+		}while(temp != null);
+		
+		return info += " ]";
 	}
 	
 	
--- estruturaDeDados/src/implementacaoListaComEncadeamento/ListaComEncadeamentoMain.java
+++ estruturaDeDados/src/implementacaoListaComEncadeamento/ListaComEncadeamentoMain.java
@@ -3,18 +3,31 @@ package implementacaoListaComEncadeamento;
 public class ListaComEncadeamentoMain {
 	public static void main(String[] args) {
 		ListaComEncadeamento lista = new ListaComEncadeamento();
-		lista.add(23);		
+		lista.add(23);
+		//99 -> 1
 		lista.add(12);
 		lista.add(53);
-		lista.add(83);
-		lista.add(43);
+		lista.add(99);
+		lista.add(99);
+		lista.add(12);
+		lista.add(99);
 		// dps -> null
 		// this.last = 5, this.last.getNext -> null 
 		// p/ novo, qualquer dado, ultima posicao o getNext -> null
 		// novo = info, null
-		// 
-		//lista.add(99, 1)
+		//
+		System.out.println(lista);
 		
-		lista.exibirLista();
+		
+		//System.out.println(lista.removeFirst(53));
+		//System.out.println(lista.get(2));
+		//System.out.println(lista.set(2, 99));
+		//System.out.println(lista.contains(99));
+		//System.out.println(lista.indexOf(99));
+		System.out.println(lista.lastIndexOf(99));
+		
+		System.out.println(lista);
+		
+		//System.out.println(lista.toArray());
 	}
 }
